5f1a94ffe4222e9bf83d86b855e15b527809d11e
Diego Urrego <diego.urrego@mercadolibre.com.co>
2024-03-06 17:36:19 -0500

    feat: handler tests for FindByBrandAndYearRange method implemented

--- internal/handler/vehicle_test.go
+++ internal/handler/vehicle_test.go
@@ -113,3 +113,151 @@ func TestHandlerVehicle_FindByColorAndYear(t *testing.T) {
 		sv.AssertExpectations(t)
 	})
 }
+
+func TestHandlerVehicle_FindByBrandAndYearRange(t *testing.T) {
+	t.Run("success - case 01: should returns a list with vehicles", func(t *testing.T) {
+		// Arrange
+		carBrand := "Chevrolet"
+		startYear := 1995
+		endYear := 1997
+		sv := service.NewVehicleDefaultMock()
+		expectedResult := map[int]internal.Vehicle{
+			11: {
+				Id: 11,
+				VehicleAttributes: internal.VehicleAttributes{
+					Brand:           "Chevrolet",
+					Model:           "G-Series 2500",
+					Registration:    "9292",
+					Color:           "Mauv",
+					FabricationYear: 1996,
+					Capacity:        3,
+					MaxSpeed:        239,
+					FuelType:        "gas",
+					Transmission:    "manual",
+					Weight:          152.87,
+					Dimensions: internal.Dimensions{
+						Height: 50.84,
+						Length: 0,
+						Width:  216.53,
+					},
+				},
+			},
+			14: {
+				Id: 14,
+				VehicleAttributes: internal.VehicleAttributes{
+					Brand:           "Chevrolet",
+					Model:           "Suburban 2500",
+					Registration:    "051",
+					Color:           "Pink",
+					FabricationYear: 1997,
+					Capacity:        5,
+					MaxSpeed:        173,
+					FuelType:        "gas",
+					Transmission:    "automatic",
+					Weight:          65.95,
+					Dimensions: internal.Dimensions{
+						Height: 40.51,
+						Length: 0,
+						Width:  135.28,
+					},
+				},
+			},
+		}
+		sv.On("FindByBrandAndYearRange", carBrand, startYear, endYear).Return(expectedResult, nil)
+		hd := NewHandlerVehicle(sv)
+		hdFunc := hd.FindByBrandAndYearRange()
+
+		// Act
+		req := httptest.NewRequest(http.MethodGet, "/vehicles/brand/Chevrolet/between/1995/1997", nil)
+		chiCtx := chi.NewRouteContext()
+		chiCtx.URLParams.Add("brand", carBrand)
+		chiCtx.URLParams.Add("start_year", "1995")
+		chiCtx.URLParams.Add("end_year", "1997")
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, chiCtx))
+		res := httptest.NewRecorder()
+		hdFunc(res, req)
+
+		// Assert
+		expectedCode := http.StatusOK
+		expectedBody := `{
+		   "data": {
+		       "11": {
+		           "Id": 11,
+		           "Brand": "Chevrolet",
+		           "Model": "G-Series 2500",
+		           "Registration": "9292",
+		           "Color": "Mauv",
+		           "FabricationYear": 1996,
+		           "Capacity": 3,
+		           "MaxSpeed": 239,
+		           "FuelType": "gas",
+		           "Transmission": "manual",
+		           "Weight": 152.87,
+		           "Height": 50.84,
+		           "Length": 0,
+		           "Width": 216.53
+		       },
+		       "14": {
+		           "Id": 14,
+		           "Brand": "Chevrolet",
+		           "Model": "Suburban 2500",
+		           "Registration": "051",
+		           "Color": "Pink",
+		           "FabricationYear": 1997,
+		           "Capacity": 5,
+		           "MaxSpeed": 173,
+		           "FuelType": "gas",
+		           "Transmission": "automatic",
+		           "Weight": 65.95,
+		           "Height": 40.51,
+		           "Length": 0,
+		           "Width": 135.28
+		       }
+		   },
+		   "message": "vehicles found"
+		}`
+		expectedHeader := http.Header{
+			"Content-Type": []string{"application/json; charset=utf-8"},
+		}
+
+		require.Equal(t, expectedCode, res.Code)
+		require.JSONEq(t, expectedBody, res.Body.String())
+		require.Equal(t, expectedHeader, res.Header())
+		sv.AssertExpectations(t)
+
+	})
+
+	t.Run("failure- case01: should returns a not found error", func(t *testing.T) {
+		// Arrange
+		carBrand := "Chevrolet"
+		startYear := 2023
+		endYear := 2024
+		sv := service.NewVehicleDefaultMock()
+		expectedResult := map[int]internal.Vehicle{}
+		sv.On("FindByBrandAndYearRange", carBrand, startYear, endYear).Return(expectedResult, internal.ErrRepositoryNotFound)
+		hd := NewHandlerVehicle(sv)
+		hdFunc := hd.FindByBrandAndYearRange()
+
+		// Act
+		req := httptest.NewRequest(http.MethodGet, "/vehicles/brand/Chevrolet/between/2023/2024", nil)
+		chiCtx := chi.NewRouteContext()
+		chiCtx.URLParams.Add("brand", carBrand)
+		chiCtx.URLParams.Add("start_year", "2023")
+		chiCtx.URLParams.Add("end_year", "2024")
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, chiCtx))
+		res := httptest.NewRecorder()
+		hdFunc(res, req)
+
+		// Assert
+		expectedCode := http.StatusNotFound
+		expectedBody := `{"status":"Not Found","message":"No vehicles were found for the provided criteria"}`
+		expectedHeader := http.Header{
+			"Content-Type": []string{"application/json"},
+		}
+
+		require.Equal(t, expectedCode, res.Code)
+		require.JSONEq(t, expectedBody, res.Body.String())
+		require.Equal(t, expectedHeader, res.Header())
+		sv.AssertExpectations(t)
+	})
+}
